9407bb3cf1638cdcb655a31e85fb6160f9ecb557
Aman Gupta <aman@tmm1.net>
2013-10-26 02:00:04 -0700

    ensure MRI apis are available

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,2 +1,9 @@
 require 'mkmf'
-create_makefile('stackprof')
+if have_func('rb_postponed_job_register_one') &&
+   have_func('rb_profile_frames') &&
+   have_func('rb_tracepoint_new') &&
+   have_const('RUBY_INTERNAL_EVENT_NEWOBJ')
+  create_makefile('stackprof')
+else
+  fail 'missing API: are you using ruby 2.1+?'
+end
