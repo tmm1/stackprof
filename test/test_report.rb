c247c7379829f5474963ac5c4319036039dbe5f5
Peter Suschlik <ps@neopoly.de>
2015-01-06 16:31:22 +0100

    Pass optional io to Report#print_dump
    After this change you can dump your profile results to any IO via:
    
        report = StackProf::Report.new(StackProf.results)
        File.open("profile.dump", "wb") do |io|
          report.print_dump(io)
        end

--- test/test_report.rb
+++ test/test_report.rb
@@ -3,6 +3,8 @@ require 'stackprof'
 require 'minitest/autorun'
 
 class ReportDumpTest < MiniTest::Test
+  require 'stringio'
+
   def test_dump_to_stdout
     data = {}
     report = StackProf::Report.new(data)
@@ -12,6 +14,16 @@ class ReportDumpTest < MiniTest::Test
     end
 
     assert_dump data, out
+  end
+
+  def test_dump_to_file
+    data = {}
+    f = StringIO.new
+    report = StackProf::Report.new(data)
+
+    report.print_dump(f)
+
+    assert_dump data, f.string
   end
 
   private
