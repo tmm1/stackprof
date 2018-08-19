213b8e6daca3b042ca49970bdb4a4f8eaf003ac4
Jamie Wong <jlfwong@figma.com>
2018-08-19 12:32:12 -0700

    Add missing txt file

--- vendor/speedscope/update.sh
+++ vendor/speedscope/update.sh
@@ -10,7 +10,7 @@ tar -xvvf speedscope-*.tgz
 # Replace the existing sources with the sources contained by the tarball
 rm -rf speedscope
 mkdir speedscope
-mv package/LICENSE package/dist/release/*.{html,css,js,png} speedscope
+mv package/LICENSE package/dist/release/*.{html,css,js,png,txt} speedscope
 
 # Clean up
 rm -rf package speedscope-*.tgz
