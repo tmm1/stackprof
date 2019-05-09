eccde8ae7c459cec80736edef173ef5bccdc511c
Andrew Metcalf <andrew@stripe.com>
2019-05-08 17:02:55 -0700

    Add a test for Report#+

--- test/test_report.rb
+++ test/test_report.rb
@@ -2,7 +2,7 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 require 'stackprof'
 require 'minitest/autorun'
 
-class ReportDumpTest < MiniTest::Test
+class ReportTest < MiniTest::Test
   require 'stringio'
 
   def test_dump_to_stdout
@@ -26,7 +26,36 @@ class ReportDumpTest < MiniTest::Test
     assert_dump data, f.string
   end
 
+  def test_add
+    p1 = StackProf.run(mode: :cpu, raw: true) do
+      foo
+    end
+    p2 = StackProf.run(mode: :cpu, raw: true) do
+      foo
+      bar
+    end
+
+    combined = StackProf::Report.new(p1) + StackProf::Report.new(p2)
+
+    assert_equal 2, find_method_frame(combined, :foo)[:total_samples]
+    assert_equal 1, find_method_frame(combined, :bar)[:total_samples]
+  end
+
   private
+
+  def find_method_frame(profile, name)
+    profile.frames.values.find do |frame|
+      frame[:name] == "ReportTest##{name}"
+    end
+  end
+
+  def foo
+    StackProf.sample
+  end
+
+  def bar
+    StackProf.sample
+  end
 
   def assert_dump(expected, marshal_data)
     assert_equal expected, Marshal.load(marshal_data)
