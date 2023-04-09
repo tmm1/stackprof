a1f5898c7bca704f3e7ea4bbd23bbbd86e8b90df
Dale Hamel <dale.hamel@shopify.com>
2023-04-08 23:02:16 -0400

    Support for tag persistence/inheritence to children

--- lib/stackprof/tags.rb
+++ lib/stackprof/tags.rb
@@ -6,7 +6,7 @@ module StackProf
 
     class << self
       def with(tag_source: DEFAULT_TAG_SOURCE, **tags, &block)
-        before = current(tag_source: tag_source)
+        before = check(tag_source: tag_source)
         set(**tags, tag_source: tag_source) # TODO push this onto a stack rather than set / unset, or we will clobber old values
         yield
         Thread.current[tag_source] = before
@@ -26,8 +26,51 @@ module StackProf
         Thread.current[tag_source].clear if Thread.current[tag_source].is_a?(Hash)
       end
 
-      def current(tag_source: DEFAULT_TAG_SOURCE)
+      def check(tag_source: DEFAULT_TAG_SOURCE)
         Thread.current.fetch(tag_source, {}).dup
+      end
+    end
+
+
+    # Persistence is a global API to toggle inheriting tags from parent thread
+    # It does this by monkey-patching Thread.new, however, which is why this
+    # defaults to disabled
+    module Persistence
+      extend self
+
+      attr_reader :enabled
+
+      def enable
+        @enabled ||= true
+        @prepended ||= begin
+          Thread.singleton_class.prepend(StackProf::Tag::ExtendedThread)
+          true
+        end
+      end
+
+      def disable
+        #puts "Disabling tag persistence"
+        @enabled = false
+      end
+    end
+
+    # ExtendedThread wraps Thread.new constructor in order to toggle inheritence
+    # of a specific thread local value
+    module ExtendedThread
+      def new(*args, &block)
+        return super(*args, &block) unless StackProf::Tag::Persistence.enabled
+        wrap_block = begin
+          thread_vars = Thread.current.fetch(DEFAULT_TAG_SOURCE, nil) # FIXME read the tag source from module variable, don't assume it is default
+          if thread_vars.is_a?(Hash) && !thread_vars.empty?
+            wrap_block = Proc.new do
+              Thread.current[DEFAULT_TAG_SOURCE] = thread_vars.dup
+              block.call
+            end
+          else
+            block
+          end
+        end
+        super(*args, &wrap_block)
       end
     end
   end
---
+++ test/test_stackprof_tags.rb
@@ -0,0 +1,210 @@
+$:.unshift File.expand_path('../../lib', __FILE__)
+require 'stackprof'
+require 'minitest/autorun'
+
+class StackProfTagsTest < MiniTest::Test
+  def teardown
+    StackProf::Tag.clear
+    StackProf::Tag::Persistence.disable
+  end
+
+  def test_tag_thread_id
+    profile = StackProf.run(mode: :cpu, tags: [:thread_id]) do # FIXME try :wall to make tests faster
+      math(100) # FIXME way too big
+    end
+
+    assert_equal true, profile.key?(:sample_tags)
+    assert_equal profile[:samples], profile[:sample_tags].size
+    assert_operator profile[:sample_tags].size, :>, 0
+    assert_equal true, profile[:sample_tags].all? { |t| t.key?(:thread_id)}
+    assert_equal true, profile[:sample_tags].all? { |t| Thread.current.to_s.include?(t[:thread_id])}
+  end
+
+  def test_tag_with_helper
+    profile = StackProf.run(mode: :cpu, tags: [:foo]) do
+      math(10)
+      StackProf::Tag.with(foo: :bar) do
+        math(10)
+      end
+      math(10)
+    end
+    #STDERR.puts "PROF #{profile[:sample_tags].inspect}"
+    assert_equal true, profile.key?(:sample_tags)
+    assert_equal profile[:samples], profile[:sample_tags].size
+    assert_operator profile[:sample_tags].size, :>, 0
+    assert_equal true, tag_order_matches(profile, [{}, {foo: :bar}, {}])
+  end
+
+  def test_tag_sample_from_tag_source_with_multiple_threads
+    main_id = sub_id = ""
+    profile = StackProf.run(mode: :cpu, tags: [:thread_id, :foo]) do
+      main_id = parse_thread_id(Thread.current)
+      StackProf::Tag.set(foo: :bar)
+      Thread.new do
+        sub_id = parse_thread_id(Thread.current)
+        StackProf::Tag.set(foo: :baz)
+        math(10)
+      end.join
+      math(10)
+      StackProf::Tag.clear
+      math(10)
+    end
+
+    assert_equal true, profile.key?(:sample_tags)
+    assert_equal profile[:samples], profile[:sample_tags].size
+    assert_operator profile[:sample_tags].size, :>, 0
+    assert_equal true, profile[:sample_tags].all? { |t| t.key?(:thread_id) }
+
+    assert_equal true, tag_order_matches(profile, [{thread_id: sub_id, foo: :baz}, {thread_id: main_id, foo: :bar}, {thread_id: main_id}])
+  end
+
+
+  def test_tag_samples_with_tags_as_closure
+    main_id = sub_id = ""
+    profile = StackProf.run(mode: :cpu, tags: [:foo, :spam]) do
+      main_id = parse_thread_id(Thread.current)
+      math(10)
+      StackProf::Tag.with(foo: :bar) do
+        math(10)
+        StackProf::Tag.with(foo: :baz) do
+          math(10)
+          StackProf::Tag.with(spam: :eggs) do
+            math(10)
+          end
+          math(10)
+        end
+        math(10)
+      end
+      math(10)
+    end
+
+    assert_equal true, profile.key?(:sample_tags)
+    assert_equal profile[:samples], profile[:sample_tags].size
+    assert_operator profile[:sample_tags].size, :>, 0
+    #STDERR.puts "PROF #{profile[:sample_tags].inspect}"
+    assert_equal true, tag_order_matches(profile, [{}, {foo: :bar}, {foo: :baz}, {foo: :baz, spam: :eggs}, {foo: :baz}, {foo: :bar}, {}])
+  end
+
+
+  def test_tag_sample_from_custom_tag_source
+    custom_tag_source = :my_custom_tag_source
+    StackProf::Tag.set(foo: :bar, tag_source: custom_tag_source)
+    profile = StackProf.run(mode: :cpu, tags: [:foo], tag_source: custom_tag_source) do
+      math(10)
+    end
+
+    assert_equal true, profile.key?(:sample_tags)
+    assert_equal profile[:samples], profile[:sample_tags].size
+    assert_operator profile[:sample_tags].size, :>, 0
+    assert_equal true, profile[:sample_tags].all? { |t| t[:foo] == :bar }
+  end
+
+  def test_tag_sample_with_symbol_or_string
+    StackProf::Tag.set(foo: :bar, spam: "a lot")
+
+    profile = StackProf.run(mode: :cpu, tags: [:foo, :spam]) do
+      math(10)
+    end
+
+    assert_equal true, profile.key?(:sample_tags)
+    assert_equal profile[:samples], profile[:sample_tags].size
+    assert_operator profile[:sample_tags].size, :>, 0
+    assert_equal true, profile[:sample_tags].all? { |t| t[:foo] == :bar }
+    assert_equal true, profile[:sample_tags].all? { |t| t[:spam] == "a lot" }
+  end
+
+  def parse_thread_id(thread)
+    thread.to_s.scan(/#<Thread:(.*?)\s.*>/).flatten.first
+  end
+
+  # TODO build up a debug string to print and call from ensure clause
+  def tag_order_matches(profile, order)
+    debugstr = ""
+    rc = false
+    return rc if order.size < 1
+    idx = 0
+    acceptable = nil
+    profile.fetch(:sample_tags, []).each do |tags|
+      acceptable = order[idx]
+      if tags != acceptable && idx < order.size
+        idx = idx + 1
+        next_acceptable = order[idx]
+        debugstr += sprintf("%02d/%02d) %s != %s, next %s\n", idx, order.size, tags, acceptable, next_acceptable)
+        if tags != next_acceptable
+          break
+        end
+        acceptable = next_acceptable
+      end
+    end
+    rc = idx == (order.size - 1)
+  ensure
+    puts debugstr unless rc
+  end
+
+  def test_sample_tag_persistence_from_parent
+    StackProf::Tag::Persistence.enable
+    assert_equal true, StackProf::Tag::Persistence.enabled
+
+    main_id = parse_thread_id(Thread.current)
+    sub_id = ""
+
+    StackProf::Tag.set(foo: :bar, spam: :eggs)
+    assert_operator StackProf::Tag.check, :==, {foo: :bar, spam: :eggs}
+
+    profile = StackProf.run(mode: :cpu, tags: [:thread_id, :foo, :spam]) do
+      Thread.new do
+        StackProf::Tag.set(foo: :baz)
+        assert_operator StackProf::Tag.check, :==, {foo: :baz, spam: :eggs}
+        sub_id = parse_thread_id(Thread.current)
+        math(1)
+      end.join
+      math(1)
+      StackProf::Tag.clear
+      assert_operator StackProf::Tag.check, :==, {}
+      math(1)
+    end
+
+    assert_equal true, profile.key?(:sample_tags)
+    assert_equal profile[:samples], profile[:sample_tags].size
+    assert_operator profile[:sample_tags].size, :>, 0
+    assert_equal true, profile[:sample_tags].all? { |t| t.key?(:thread_id) }
+
+    #STDERR.puts "PROF #{profile[:sample_tags].inspect}"
+    assert_equal true, tag_order_matches(profile, [{thread_id: sub_id, foo: :baz, spam: :eggs}, {thread_id: main_id, foo: :bar, spam: :eggs}, {thread_id: main_id}])
+
+    # Now let's disable it and verify things are back to normal
+    StackProf::Tag.set(foo: :bar, spam: :eggs)
+    assert_operator StackProf::Tag.check, :==, {foo: :bar, spam: :eggs}
+
+    StackProf::Tag::Persistence.disable
+    assert_equal false, StackProf::Tag::Persistence.enabled
+
+    profile = StackProf.run(mode: :cpu, tags: [:thread_id, :foo, :spam]) do
+      Thread.new do
+        StackProf::Tag.set(foo: :baz)
+        assert_operator StackProf::Tag.check, :==, {foo: :baz}
+        sub_id = parse_thread_id(Thread.current)
+        math(10)
+      end.join
+      math(10)
+      StackProf::Tag.clear
+      assert_operator StackProf::Tag.check, :==, {}
+      math(10)
+    end
+
+    assert_equal true, profile.key?(:sample_tags)
+    assert_equal profile[:samples], profile[:sample_tags].size
+    assert_operator profile[:sample_tags].size, :>, 0
+    assert_equal true, profile[:sample_tags].all? { |t| t.key?(:thread_id) }
+
+    #STDERR.puts "PROF #{profile[:sample_tags].inspect}"
+    assert_equal true, tag_order_matches(profile, [{thread_id: sub_id, foo: :baz}, {thread_id: main_id, foo: :bar, spam: :eggs}, {thread_id: main_id}])
+  end
+
+  def math(n)
+    base = 250_000
+    (n * base).times do
+      2 ** 10
+    end
+  end
+end
