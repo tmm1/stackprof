f48040896198ae111752514fdc3a709c334578a2
Dale Hamel <dale.hamel@shopify.com>
2023-04-08 23:15:25 -0400

    Style fixes

--- lib/stackprof/tags.rb
+++ lib/stackprof/tags.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 module StackProf
   module Tag
-    # TODO add a way to inherit tags from parent thread by wrapping Thread.new and Fiber.new
+    # TODO: add a way to inherit tags from parent thread by wrapping Thread.new and Fiber.new
     # in a module, and include this module to monkey-patch them with these wrappers.
     # work by passing all of Thread.current.keys with values to Thread.new
 
     class << self
-      def with(tag_source: DEFAULT_TAG_SOURCE, **tags, &block)
+      def with(tag_source: DEFAULT_TAG_SOURCE, **tags)
         before = check(tag_source: tag_source)
-        set(**tags, tag_source: tag_source) # TODO push this onto a stack rather than set / unset, or we will clobber old values
+        set(**tags, tag_source: tag_source) # TODO: push this onto a stack rather than set / unset, or we will clobber old values
         yield
         Thread.current[tag_source] = before
       end
@@ -19,6 +21,8 @@ module StackProf
 
       def unset(*tags, tag_source: DEFAULT_TAG_SOURCE)
         return unless Thread.current[tag_source].is_a?(Hash)
+
+        # FIXME - instead of iterating, build up the dictionary then set it all at once or tags could be inconsistent if a snapshot happens while we are updating
         tags.each { |tag| Thread.current[tag_source].delete(tag) }
       end
 
@@ -31,10 +35,11 @@ module StackProf
       end
     end
 
-
-    # Persistence is a global API to toggle inheriting tags from parent thread
+    # Persistence provides a singleton to toggle inheriting tags from parent thread
     # It does this by monkey-patching Thread.new, however, which is why this
     # defaults to disabled
+    # Tag Persistence is useful if you want to ensure that tags set globally
+    # will show up in all samples, regardless of what thread is being recorded
     module Persistence
       extend self
 
@@ -49,7 +54,7 @@ module StackProf
       end
 
       def disable
-        #puts "Disabling tag persistence"
+        # puts "Disabling tag persistence"
         @enabled = false
       end
     end
@@ -59,10 +64,11 @@ module StackProf
     module ExtendedThread
       def new(*args, &block)
         return super(*args, &block) unless StackProf::Tag::Persistence.enabled
+
         wrap_block = begin
-          thread_vars = Thread.current.fetch(DEFAULT_TAG_SOURCE, nil) # FIXME read the tag source from module variable, don't assume it is default
+          thread_vars = Thread.current.fetch(DEFAULT_TAG_SOURCE, nil) # FIXME: read the tag source from module variable, don't assume it is default
           if thread_vars.is_a?(Hash) && !thread_vars.empty?
-            wrap_block = Proc.new do
+            wrap_block = proc do
               Thread.current[DEFAULT_TAG_SOURCE] = thread_vars.dup
               block.call
             end
